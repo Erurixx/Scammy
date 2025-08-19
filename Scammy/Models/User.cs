91d2d88a1e1dbf314d70637c4a64ebfb113ea2a0
JASMINE CHONG QI EN <TP064426@mail.apu.edu.my>
2025-08-19 23:47:18 +0800

    can login and register

---
+++ Scammy/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Scammy.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string FullName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; }  // Store hashed password!
+
+        [MaxLength(20)]
+        public string UserRole { get; set; } = "jobseeker";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
