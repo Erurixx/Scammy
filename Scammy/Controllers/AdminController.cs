091a17c865b7e2d41f12c1a17bbd8dedf602aa45
HO KAY LEE <TP065034@mail.apu.edu.my>
2025-08-21 10:56:58 +0800

    kl

---
+++ Scammy/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scammy.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View("AdminDashboard");
+        }
+
+        public ActionResult ManageReport()
+        {
+            return View("ManageReport");
+        }
+
+        public ActionResult ManageUser()
+        {
+            return View("ManageUser");
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View("CreateUser");
+        }
+    }
+}
